8db31e790f721c1291b40c86897001410c0ad326
batyrkhana <batyrkhansa@gmail.com>
2023-10-04 09:24:43 -0400

    3

--- index.js
+++ index.js
@@ -3,3 +3,48 @@ function BMICalculator (user_height_meters, user_weight) {
 }
 let user_height = Number(prompt("Please enter your height in centimeters:"))
 let user_weight = Number(prompt("Please enter your weight in kilograms:"))
+
+/* function getBMI(x,y){
+    return 20;
+} */
+
+
+/* // conditional statements if/else if/else
+function check(x,y){
+    if(x === "horse") {
+        console.log(true);
+    }
+    else {
+        console.log(false);
+    }
+}
+//check("horsey")
+function complex(x,y){
+    if(x === "horse" && y === "horse") {
+        console.log("that is true");
+    } 
+    else {
+        console.log("that is false");
+    }
+}
+complex("horse", "horsey");
+
+// check if x is not something
+
+function check(x,y){
+    if(x !== "horse") {
+        console.log(true);
+    }
+    else {
+        console.log(false);
+    }
+}
+//There will soon be an exam on truth tables
+// 2 equal signs == means the value
+// 3 equal signs is value and type
+
+function init(){
+    y = weight;
+    z = height;
+    let x = getBMI
+} */
